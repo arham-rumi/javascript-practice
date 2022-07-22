ee06f7f37d2add63258ec0311786cb137ea84b77
Arham Rumi <arhamrumi007@gmail.com>
2022-07-22 10:56:53 +0500

    Find if the number is in the range

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,16 +1,7 @@
 
 // ----------------------------------------------------------------
 
-// Finding if the given number is within the given range
-// let num = +prompt('Enter the number to check');
-// let start = +prompt('Enter the starting point');
-// let end = +prompt('Enter the ending point');
 
-// if (num > start && num < end){
-//     console.log(`Given number ${num} is in the range of ${start} to ${end}`);
-// }else{
-//     console.log(`Given number ${num} is not in the range of ${start} to ${end}`);
-// }
 // ----------------------------------------------------------------------------
 
 // Performing Arithmetic operations on two numbers
