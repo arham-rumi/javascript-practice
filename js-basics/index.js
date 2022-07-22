adbbc747ddf461bb1fb168f7873b25ae25e01f1a
Arham Rumi <arhamrumi007@gmail.com>
2022-07-22 10:45:02 +0500

    1st Program

---
+++ js-basics/index.js
@@ -0,0 +1,9 @@
+// Finding the maximum number of two
+// let num1 = +prompt('Enter first number : ')
+// let num2 = +prompt('Enter second number : ')
+// max = num1
+// if (num2 > num1){
+//     max = num2
+// }
+// console.log(max)
+// ---------------------------------------------------------------
