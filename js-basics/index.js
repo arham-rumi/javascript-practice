693c902f4ca8edd0cc20c487508ba3288b114974
Arham Rumi <arhamrumi007@gmail.com>
2022-07-22 11:02:20 +0500

    Find Armstrong  Number

--- js-basics/index.js
+++ js-basics/index.js
@@ -8,20 +8,7 @@
 // --------------------------------------------------------------------
 
 
-// Finding if a number is Armstrong number or not 
-// let number = +prompt('Enter a number');
-// let numStr = number.toString();
-// let armsSum = 0;
-// for (let digit of numStr){
-//     digit = Number(digit);
-//     armsSum += digit**3;
-// }
-
-// if (armsSum == number){
-//     console.log(`${number} is Armstrong number`);
-// }else{
-//     console.log(`${number} is not Armstrong number`);
-// }
+// 
 // ----------------------------------------------------------------
 
 
