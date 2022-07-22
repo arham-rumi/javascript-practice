1aafd9efd9af5ff2b2d5ec9755fdcbe9cc49417e
Arham Rumi <arhamrumi007@gmail.com>
2022-07-22 10:50:40 +0500

    Find Even or Odd

---
+++ js-basics/FindEvenOdd.js
@@ -0,0 +1,7 @@
+// Finding if the number is even or odd
+let num = +prompt('Enter a number : ')
+let flag = false
+if (num % 2 == 0) {
+    flag = true
+}
+console.log(flag)
--- js-basics/index.js
+++ js-basics/index.js
@@ -2,18 +2,6 @@
 // ---------------------------------------------------------------
 
 
-
-
-// Finding if the number is even or odd
-// let num = +prompt('Enter a number : ')
-// let flag = false
-// if (num % 2 == 0) {
-//     flag = true
-// }
-// console.log(flag)
-// ---------------------------------------------------------------
-
-
 // Finding if the input is a number or not
 // let num = prompt('Enter a number : ')
 // numFlag = false
