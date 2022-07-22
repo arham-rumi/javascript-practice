26b4df205e48ad97689998657cc8a07b2bf89e4b
Arham Rumi <arhamrumi007@gmail.com>
2022-07-22 10:53:54 +0500

    Find Triangle Type

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,21 +1,4 @@
 
-// ---------------------------------------------------------------
-
-
-
-// ----------------------------------------------------------
-
-// Finding if the triangle is equilateral, scalene, or isosceles
-// let side1 = +prompt('Enter length of Side 1:');
-// let side2 = +prompt('Enter length of Side 2:');
-// let side3 = +prompt('Enter length of Side 3:');
-// if (side1 == side2 && side2 == side3){
-//     console.log('Triangle is Equilateral');
-// }else if ((side1 == side2 || side2 == side3 || side3 == side1) && (side1 != side2 || side2 != side3 || side1 != side3)){
-//     console.log('Triangle is isosceles');
-// }else{
-//     console.log('Triangle is scalene');
-// }
 // ----------------------------------------------------------------
 
 // Finding if the given number is within the given range
